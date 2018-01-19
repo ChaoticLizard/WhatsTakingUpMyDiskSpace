843cd7cef0bad1f81fa4b640c92471827f4f2a6f
Eric Hsu <erichsu314@live.com>
2018-01-19 12:33:12 -0800

    Importing Program

---
+++ FolderSizeFinder/FolderSizeFinder/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace FolderSizeFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkedListBoxDrives = new System.Windows.Forms.CheckedListBox();
+            this.labelDrive = new System.Windows.Forms.Label();
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.dataGridViewFolderResults = new System.Windows.Forms.DataGridView();
+            this.Folder = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.Size = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.checkedListBoxDirFilter = new System.Windows.Forms.CheckedListBox();
+            this.labelDirFilter = new System.Windows.Forms.Label();
+            this.comboBoxMinMB = new System.Windows.Forms.ComboBox();
+            this.labelMinMB = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFolderResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxDrives
+            // 
+            this.checkedListBoxDrives.CheckOnClick = true;
+            this.checkedListBoxDrives.FormattingEnabled = true;
+            this.checkedListBoxDrives.Location = new System.Drawing.Point(49, 34);
+            this.checkedListBoxDrives.Name = "checkedListBoxDrives";
+            this.checkedListBoxDrives.Size = new System.Drawing.Size(160, 94);
+            this.checkedListBoxDrives.TabIndex = 0;
+            // 
+            // labelDrive
+            // 
+            this.labelDrive.AutoSize = true;
+            this.labelDrive.Location = new System.Drawing.Point(49, 15);
+            this.labelDrive.Name = "labelDrive";
+            this.labelDrive.Size = new System.Drawing.Size(139, 13);
+            this.labelDrive.TabIndex = 1;
+            this.labelDrive.Text = "Select which Drives to scan";
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Location = new System.Drawing.Point(52, 466);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(75, 23);
+            this.buttonScan.TabIndex = 2;
+            this.buttonScan.Text = "Scan";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // dataGridViewFolderResults
+            // 
+            this.dataGridViewFolderResults.AllowUserToOrderColumns = true;
+            this.dataGridViewFolderResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewFolderResults.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Folder,
+            this.Size});
+            this.dataGridViewFolderResults.Location = new System.Drawing.Point(215, 34);
+            this.dataGridViewFolderResults.MultiSelect = false;
+            this.dataGridViewFolderResults.Name = "dataGridViewFolderResults";
+            this.dataGridViewFolderResults.RowHeadersWidth = 25;
+            this.dataGridViewFolderResults.Size = new System.Drawing.Size(603, 762);
+            this.dataGridViewFolderResults.TabIndex = 3;
+            // 
+            // Folder
+            // 
+            this.Folder.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Folder.HeaderText = "Folder";
+            this.Folder.Name = "Folder";
+            this.Folder.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.Folder.Width = 61;
+            // 
+            // Size
+            // 
+            this.Size.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Size.HeaderText = "Size (MB)";
+            this.Size.Name = "Size";
+            this.Size.Width = 77;
+            // 
+            // checkedListBoxDirFilter
+            // 
+            this.checkedListBoxDirFilter.FormattingEnabled = true;
+            this.checkedListBoxDirFilter.Items.AddRange(new object[] {
+            "Hidden",
+            "System",
+            "Read-Only"});
+            this.checkedListBoxDirFilter.Location = new System.Drawing.Point(52, 256);
+            this.checkedListBoxDirFilter.Name = "checkedListBoxDirFilter";
+            this.checkedListBoxDirFilter.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBoxDirFilter.TabIndex = 4;
+            // 
+            // labelDirFilter
+            // 
+            this.labelDirFilter.AutoSize = true;
+            this.labelDirFilter.Location = new System.Drawing.Point(52, 237);
+            this.labelDirFilter.Name = "labelDirFilter";
+            this.labelDirFilter.Size = new System.Drawing.Size(137, 13);
+            this.labelDirFilter.TabIndex = 5;
+            this.labelDirFilter.Text = "Types to exclude from scan";
+            // 
+            // comboBoxMinMB
+            // 
+            this.comboBoxMinMB.FormattingEnabled = true;
+            this.comboBoxMinMB.Items.AddRange(new object[] {
+            "100",
+            "1000",
+            "10000"});
+            this.comboBoxMinMB.Location = new System.Drawing.Point(55, 185);
+            this.comboBoxMinMB.Name = "comboBoxMinMB";
+            this.comboBoxMinMB.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxMinMB.TabIndex = 6;
+            // 
+            // labelMinMB
+            // 
+            this.labelMinMB.AllowDrop = true;
+            this.labelMinMB.AutoSize = true;
+            this.labelMinMB.Location = new System.Drawing.Point(52, 166);
+            this.labelMinMB.Name = "labelMinMB";
+            this.labelMinMB.Size = new System.Drawing.Size(94, 13);
+            this.labelMinMB.TabIndex = 7;
+            this.labelMinMB.Text = "Only show > X MB";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(55, 514);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 846);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.labelMinMB);
+            this.Controls.Add(this.comboBoxMinMB);
+            this.Controls.Add(this.labelDirFilter);
+            this.Controls.Add(this.checkedListBoxDirFilter);
+            this.Controls.Add(this.dataGridViewFolderResults);
+            this.Controls.Add(this.buttonScan);
+            this.Controls.Add(this.labelDrive);
+            this.Controls.Add(this.checkedListBoxDrives);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewFolderResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxDrives;
+        private System.Windows.Forms.Label labelDrive;
+        private System.Windows.Forms.Button buttonScan;
+        private System.Windows.Forms.DataGridView dataGridViewFolderResults;
+        private System.Windows.Forms.CheckedListBox checkedListBoxDirFilter;
+        private System.Windows.Forms.Label labelDirFilter;
+        private System.Windows.Forms.DataGridViewLinkColumn Folder;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Size;
+        private System.Windows.Forms.ComboBox comboBoxMinMB;
+        private System.Windows.Forms.Label labelMinMB;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ FolderSizeFinder/FolderSizeFinder/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FolderSizeFinder
+{
+    public partial class Form1 : Form
+    {
+        List<DriveInfo> allActiveDrives = new List<DriveInfo>();    // DriveInfo object and name
+        enum dirFilters {
+            Hidden = 0,
+            System,
+            ReadOnly
+        };
+        long minMB = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeData();
+        }
+
+        //
+        // Scan computer for all drives on the system and populate within listbox
+        //
+        private void InitializeData()
+        {
+            this.checkedListBoxDrives.DataSource = this.GetAvailableDrives();
+        }
+
+        private List<string> GetAvailableDrives()
+        {
+            List<string> results = new List<string>();
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach ( DriveInfo d in allDrives )
+            {
+                if ( d.IsReady )
+                {
+                    results.Add( d.Name + " (" + d.VolumeLabel + ")");
+                    allActiveDrives.Add( d );
+                }
+            }
+            return results;
+        }
+
+        //
+        // for each selected drive, scan the file system and output the size of each directory
+        //
+        private void buttonScan_Click( object sender, EventArgs e )
+        {
+            // clear any existing content in the datagrid
+            dataGridViewFolderResults.Rows.Clear();
+
+            // set the min size threshold
+            try
+            {
+                minMB = Convert.ToInt64(comboBoxMinMB.Text);
+            }
+
+            catch (System.FormatException)
+            {
+                // if invalid input or nothing is in the box, then default to 0MB
+                minMB = 0;
+            }
+            // retreive which drives to scan (use checked indices to make drive list since list is not rearranged
+            List<DirectoryInfo> drivesToScan = new List<DirectoryInfo>();
+            foreach ( int driveIndex in this.checkedListBoxDrives.SelectedIndices )
+            {
+                // interate all the directories and collect the sum of files sizes
+                DriveInfo drive = allActiveDrives[driveIndex];
+
+                // for each directory sum the size of all the files
+                DirectoryInfo dir = drive.RootDirectory;
+
+                recurseDirectories( dir );
+            }
+        }
+
+
+        //
+        // recursively get directory size and populate the size datagrid
+        //
+        private void recurseDirectories( DirectoryInfo dir )
+        {
+            long size = DirectorySize( dir );
+            size = size / 1000000;  // convert to MB
+
+            // populate the size data grid.
+            if ( size > minMB )
+            {
+                DataGridViewRow newRow = new DataGridViewRow();
+                newRow.CreateCells( dataGridViewFolderResults );
+                newRow.Cells[0].Value = dir.FullName;
+                newRow.Cells[1].Value = size;
+                dataGridViewFolderResults.Rows.Add( newRow );
+            }
+
+            // now do for all dir children
+            try
+            {
+                foreach ( DirectoryInfo d in dir.EnumerateDirectories() )
+                {
+                    // TODO: Add filtering of directories here
+                    recurseDirectories( d );
+                }
+            }
+            catch ( System.UnauthorizedAccessException )
+            {
+                // no access to that folder so skip
+            }
+            catch (System.IO.DirectoryNotFoundException)
+            {
+                // cannot find part of the path so skip
+            }
+        }
+
+        //
+        // returns the size of the directory
+        //
+        private long DirectorySize( DirectoryInfo dir )
+        {
+            long size = 0;
+            try
+            {
+                foreach ( FileInfo f in dir.EnumerateFiles() )
+                {
+                    if ( f.Attributes != FileAttributes.Directory )
+                    {
+                        size += f.Length;
+                    }
+                }
+            }
+            catch ( System.UnauthorizedAccessException )
+            {
+                // no access to that folder so will give a size of -1
+                size = -1;
+            }
+            catch ( System.IO.DirectoryNotFoundException )
+            {
+                // Cannot find "part of the path", so will give a size of -1
+                size = -1;
+            }
+            return size;
+        }
+    }
+}
